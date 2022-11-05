4717568fbb5ea54382cd5ff5fdf50660574c48d9
AmadulHaque <mdamadulhaque8@gmail.com>
2022-11-05 12:38:33 +0600

    second evercommit

--- bangladesh.js
+++ bangladesh.js
@@ -1,10 +1,18 @@
 import {laptop,mobilePhone} from './china.js';
 import {student_list} from './students.js';
+import india from './india.js';
 console.log(laptop);
+
+var product = new india();
+
+product.laotopIn();
+product.mobile();
+
+
+
 
 console.log(mobilePhone());
 console.log(student_list);
-
 student_list.map((item,i)=>{
     let student =  `Student Name ${item.name},${item.roll}`;
     console.log(student);
---
+++ india.js
@@ -0,0 +1,14 @@
+class India {
+
+    mobile(){
+        var data = ['sms_i','vivo_i','relme_i','tecno_i'];
+        console.log(data);
+    }
+
+     laotopIn(){
+        var laptop = ['asus_i','sms_i','hp_i','accer_i','dell_i'];
+        console.log(laptop);
+    }
+
+}
+export default India;
